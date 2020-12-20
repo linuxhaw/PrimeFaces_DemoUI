99891362275b1fa35deab76c9e83f35e3317662c
Zay_Bo_Hein <zay-bo-hein@gmail.com>
2020-12-20 16:16:36 +0630

    STU004	by zaybohein

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -1,5 +1,119 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.UserDTO;
+import model.UserBean;
+
 public class UserDAO {
+	public static Connection con = null;
+	static {
+		con = MyConnection.getConnection();
+	}
+
+	public int insert(UserDTO dto) {
+		int result = 0;
+		String sql = "insert into user (id,name,password) values(?,?,?)";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, dto.getId());
+			ps.setString(2, dto.getName());
+			ps.setString(3, dto.getPassword());
+			result = ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("Database error");
+		}
+		return result;
+	}
+
+	public int update(UserDTO dto) {
+		int result = 0;
+		String sql = "update user set name=?,password=? where id=?";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(3, dto.getId());
+			ps.setString(1, dto.getName());
+			ps.setString(2, dto.getPassword());
+			result = ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("Database error");
+		}
+		return result;
+	}
+
+	public int delete(UserDTO dto) {
+		int result = 0;
+		String sql = "delete from user where id=?";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, dto.getId());
+			result = ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	
+	public UserDTO select(UserDTO dto) {
+
+		try {
+			PreparedStatement p=con.prepareStatement("select * from user where id=? or name=?");
+			p.setString(1,dto.getId());
+		p.setString(2, dto.getName());
+		ResultSet rs=p.executeQuery();
+		while(rs.next()) {
+			dto.setId(rs.getString("id"));
+			dto.setName(rs.getString("name"));
+			dto.setPassword(rs.getString("password"));
+		}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dto;
+	}
+	public List<UserDTO> selectAll(){
+		List<UserDTO> dto=new ArrayList<UserDTO>();
+		try {
+			PreparedStatement p=con.prepareStatement("select * from user ");
+			ResultSet rs=p.executeQuery();
+			while(rs.next()) {
+				UserDTO res=new UserDTO();
+			res.setId(rs.getString("id"));
+			res.setName(rs.getString("name"));
+	res.setPassword(rs.getString("password"));
+
+		dto.add(res);
+			}
+		}catch(SQLException e) {
+			System.out.println("DataBase ERROR!");
+		}
+		return dto;
+	}
+	
+	public UserDTO selectOne(String id) {
+		UserDTO dto=new UserDTO();
+		try {
+			PreparedStatement p=con.prepareStatement("select * from user where id=?");
+			p.setString(1,id);
+			ResultSet rs=p.executeQuery();
+			while(rs.next()) {
+				dto.setId(rs.getString("id"));
+				dto.setName(rs.getString("name"));
+				dto.setPassword(rs.getString("password"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dto;
+	}
 
 }
--- src/model/UserBean.java
+++ src/model/UserBean.java
@@ -1,5 +1,151 @@
 package model;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.MessageInterpolator.Context;
+
+import dao.UserDAO;
+import dto.UserDTO;
+
+@ManagedBean
+@RequestScoped
 public class UserBean {
+	private String id;
+	private String name;
+	private String password;
+	private String confirm;
+
+	private UserDAO dao = new UserDAO();
+	private Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirm() {
+		return confirm;
+	}
+
+	public void setConfirm(String confirm) {
+		this.confirm = confirm;
+	}
+
+	public List<UserDTO> userList() {
+		List list = dao.selectAll();
+		return list;
+	}
+
+	public String save() {
+		UserDTO dto = new UserDTO();
+		dto.setId(id);
+		dto.setName(name);
+		dto.setPassword(password);
+		String s = " ";
+		List<UserDTO> f = dao.selectAll();
+		for (UserDTO d : f) {
+			s = d.getId();
+		}
+		if (s.equals(dto.getId())) {
+			String message = "Error";
+			return "USR002.xhtml?faces-redirect=true?message=" + message;
+		} else {
+			int result = dao.insert(dto);
+
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Message!", "Successfully Saved!"));
+			return "USR002.xhtml?faces-redirect=true";
+		}
+
+	}
+
+	public String edit(String code) {
+		UserDTO dto = new UserDTO();
+		dto.setId(code);
+		UserDTO one = dao.select(dto);
+		UserBean bean = new UserBean();
+		bean.setId(one.getId());
+		bean.setName(one.getName());
+		bean.setPassword(one.getPassword());
+		bean.setConfirm(one.getPassword());
+		sessionMap.put("editUser", bean);
+		return "USR002-01.xhtml?faces-redirect=true";
+
+	}
+
+	public String update(UserBean user) {
+		UserDTO dto = new UserDTO();
+		dto.setId(user.getId());
+		dto.setName(user.getName());
+		dto.setPassword(user.getPassword());
+		int result = dao.update(dto);
+		if (result != 0) {
+
+			FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO, "Message!", "Successfully Updated!."));
+			return "USR002-01.xhtml?faces-redirect=true";
+		} else {
+
+			return "USR002-01.xhtml?faces-redirect=true";
+		}
+	}
+
+	public String select() {
+		UserDTO d = new UserDTO();
+		d.setId(id);
+		d.setName(name);
+		UserDTO dto = dao.select(d);
+		return "USR001.xhtml?faces-redirect=true";
+	}
+
+	public void delete(String code) {
+		UserDTO dto = new UserDTO();
+		dto.setId(code);
+		dao.delete(dto);
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Save!", "Successfully Deleted!."));
+	}
+	//,HttpServletRequest request
+	public String login(UserBean user,HttpServletRequest request) {
+		UserDTO u1= new UserDTO();
+		u1=dao.selectOne(user.getId());
+		if (u1.getId()==null) {
+			FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_WARN, "Message!", "User Not found"));
+			return "";
+		}
+		if (u1.getPassword().equals(user.getPassword())) {
+			request.getSession().setAttribute("sesUser", user);
+			return "index.xhtml?faces-redirect=true";
+		}else {
+			FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_WARN, "Message!", "Password not correct"));
+			return "";
+		}
+		
+	}
 
 }
